88fbe1a98c772ec110ab20961cd4906c31e58459
Kurbatova Olga <kurbatovaovl@gmail.com>
2021-01-16 14:04:19 +0400

    initial Rest

--- src/main/java/org/example/Lab3Spring/repos/DepartmentRepo.java
+++
@@ -1,8 +0,0 @@
-//package org.example.Lab3Spring.repos;
-//
-//import org.example.Lab3Spring.entity.Department;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface DepartmentRepo
-//    extends CrudRepository<Department, Long>{
-//    }
